896c0ab298196454ff39b5f2b33caba67765ecc6
Marco Vittorini <marco.vittorini@wfp.org>
2012-04-10 16:04:04 +0200

    - added the possibility to start the installation procedure starting from an input step number

--- scripts/0_install.bat
+++ scripts/0_install.bat
@@ -13,6 +13,12 @@ if [%2]==[] (
 	goto:eof
 )
 
+if [%3]==[] (
+	set STEP=1
+) else (
+	set STEP=%3
+)
+
 set STATION=%1
 set TIMESTAMP=%2
 set /p VERSION=<..\VERSION
@@ -20,30 +26,43 @@ set /p VERSION=<..\VERSION
 echo Installing ETS-COMPAS version: %VERSION%
 echo Station is: %STATION%
 echo Timestamp is: %TIMESTAMP%
+echo Requested starting step: %STEP%
 
 @echo on
 
-echo STEP 1: Export
-call 1_full_export.bat %STATION% %TIMESTAMP%
-pause
+@if 1 geq %STEP% (
+	echo STEP 1: Export
+	call 1_full_export.bat %STATION% %TIMESTAMP%
+	pause
+)
 
-echo STEP 2: Create TESTQ
-call 2_create_testq.bat %STATION%
-pause
+@if 2 geq %STEP% (
+	echo STEP 2: Create TESTQ
+	call 2_create_testq.bat %STATION%
+	pause
+)
 
-echo STEP 3: Import
-call 3_import.bat %STATION% %TIMESTAMP%
-pause
+@if 3 geq %STEP% (
+	echo STEP 3: Import
+	call 3_import.bat %STATION% %TIMESTAMP%
+	pause
+)
 
-echo STEP 4: Fix import
-call 4_fix.bat %STATION% 
-pause
+@if 4 geq %STEP% (
+	echo STEP 4: Fix import
+	call 4_fix.bat %STATION% 
+	pause
+)
 
-echo STEP 5: Create new objects
-call 5_create_testq_newobjs.bat %STATION% %VERSION%
-pause
+@if 5 geq %STEP% (
+	echo STEP 5: Create new objects
+	call 5_create_testq_newobjs.bat %STATION% %VERSION%
+	pause
+)
 
-echo STEP 6: Create synonyms
-call 6_create_testw_syn.bat %STATION%
+@if 6 geq %STEP% (
+	echo STEP 6: Create synonyms
+	call 6_create_testw_syn.bat %STATION%
+)
 
 echo End.
